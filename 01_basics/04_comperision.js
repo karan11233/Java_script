1efddfbd958e5c336e3c2487dba7f37868212cc1
karan11233 <karanchavda953@gmail.com>
2025-02-03 16:21:18 +0530

    comparesion

---
+++ 01_basics/04_comperision.js
@@ -0,0 +1,21 @@
+//console.log(true < false);  // because true = 1 and false = 0.
+//console.log("karan" > "karana");
+//console.log(true >= !false);
+
+
+
+// === it's called strict check it will compare value as well as he data type of it
+
+// Ex:- 
+
+console.log("2" == 2); // it will give true as o/p
+console.log("2" === 2);// it will give false as o/p
+
+
+ 
+ 
+
+
+
+
+
