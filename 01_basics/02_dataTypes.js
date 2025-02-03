12ed64d2e5fceb10153fd65904661045644b9b3c
karan11233 <karanchavda953@gmail.com>
2025-02-03 14:55:50 +0530

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // it treats all js code in newer version 
+
+// alert(3+3) // Not possible
+/* Error: ReferenceError: alert is not defined
+We are using node not browser. it will only work in browser 
+*/
+
+
+/* Data type  (primitive)
+
+Type        values                  Ex
+
+number                              => Ex:- let a = 123
+bigint                              => Ex:- let s = 3234434534535345345345345 
+string      => ""                   => Ex:- let s = "Karan"
+boolean     => true/false           => Ex:- let a = true
+null        => standalone value     => Ex:- let n = null
+undefine                            => Ex:- let a;
+symbol      => unique 
+
+*/
+
+Note: //Null is an object in js 
+
+
+//Object
+console.log(typeof"karan"); // used to know type of a variable 
+console.log(typeof null); // object 
+
+
+
