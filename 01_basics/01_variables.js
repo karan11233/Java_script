2d778ed921f46fff50cc207e2fe1667599aaf626
karan11233 <karanchavda953@gmail.com>
2025-02-03 12:22:19 +0530

    basic concepts

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 123;
+let accountEmail = "karan@Google.com";
+var AccountPassword = "121345"
+accountCity = "surat"
+
+
+// accountId = 123     // you can not change the value of const variable 
+accountEmail = "karan.com"
+AccountPassword = "1211"
+accountCity = "vadodara"
+
+
+/*
+Prefer not to use var 
+because of the issue in block scope and functional scope. 
+*/
+
+// console.log(accountEmail);
+// console.log(AccountPassword);
+// console.log(accountCity);
+
+console.table([accountId, accountEmail, AccountPassword, accountCity])
