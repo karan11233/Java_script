9fced33d761931327e179e910239fb41005638d7
karan11233 <karanchavda953@gmail.com>
2025-02-03 16:04:33 +0530

    conversionalOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,6 @@
-let score = 33
+// let score = 33
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 
 /*conversion rule and notes
@@ -15,10 +15,10 @@ false               0               integer
 
 */
 
-let isLoggedIn = undefined
+// let isLoggedIn = undefined
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 /* For boolean conversion 
 
@@ -32,7 +32,34 @@ null        false
 undefined   false
 */
 
-let sumNumber = 33
+// let sumNumber = 33
 
-let stringNumber = String(sumNumber)
-console.log(typeof(stringNumber))
+// let stringNumber = String(sumNumber)
+// console.log(typeof(stringNumber))
+
+
+// *********************************Operation*****************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+
+let str1 = "Hello "
+let str2 = "Karan"
+let str3 = str1 + str2
+//console.log(str3)
+
+
+let a = "2"
+let b = 2
+let c = a+b
+let d = a*b
+//console.log(typeof(a))
+//console.log(typeof(b))
+//console.log(c)
+//console.log(d)
+//console.log(typeof(c))
+//console.log(typeof(d))
+
+console.log(-true)
