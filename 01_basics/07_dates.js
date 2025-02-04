4fc5e39013202914306f450407098393862c1fa0
karan11233 <karanchavda953@gmail.com>
2025-02-04 14:44:11 +0530

    date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,13 @@
+// Dates
+
+
+const currentDate = new Date()
+// console.log(currentDate.toString()); // print day, date and time as well                
+// console.log(currentDate.toDateString()); // print only day and date 
+// console.log(currentDate.toLocaleTimeString()); // print only time  
+// console.log(currentDate.toLocaleDateString()); // print only date in numeric formate 
+// console.log(typeof currentDate);
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
