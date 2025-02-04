fe18c54c12d846bca9e2ef2c14c54ac40739011f
karan11233 <karanchavda953@gmail.com>
2025-02-04 12:31:14 +0530

    math

--- 01_basics/06_number_maths.js
+++ 01_basics/06_number_maths.js
@@ -39,7 +39,28 @@ console.log(Math.round((Math.random()*10)))
 
 
 
+// ----- Maths in JS ----
+// Maths library comes along with JS
 
+// Methods ----
+// 1. Math.abs() // Converts +ve / -ve integer values to positive
+// 2. Math.round(4.3) // Output - 4
+// 3. Math.round(4.6) // Output - 5
+// 4. Math.ceil(4.2) // Output - 5 (gives top value)
+// 5. Math.floor(4.9) // Output - 4 (gives bottom value)
+// 6. Math.min(4,3,6,8) // Output - 3
+// 7. Math.max(4,3,6,8) // Output - 8
+// 8. Math.random() // Gives random value between 0 & 1 in decimals
+
+// Math.random() tricks -----
+// Math.random()*10
+// Math.random()*10 + 1 // This assures that values are atleast 1 & more than 1
+// (Math.random()*10) + 1 // To avoid any BODMAS rule miscalculation
+
+// ---- Trick to randomize value between range ----
+// const min = 10
+// const max = 20
+// const randomValue = Math.floor((Math.random() * (max - min + 1)) + min)
 
 
 
