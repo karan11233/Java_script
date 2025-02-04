125e35b969b74b71434ce07f140cafe8cac60fac
karan11233 <karanchavda953@gmail.com>
2025-02-04 10:35:20 +0530

    memory

---
+++ 01_basics/datatypes-summury.js
@@ -0,0 +1,63 @@
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number    => number
+//        String    => string
+//        Boolean   => boolean
+//        null      => object
+//        undefined => undefined
+//        Symbol    => symbol
+//        BigInt    => bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays    => object
+//        Function  => function
+//        Object    => object
+
+
+// Primitive 
+// 7 Types: String, Number, Boolean, null, undefined, Symbol, BigInt 
+
+const score = 7
+const scoreValue = 7.5
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+//console.log(id === anotherId);
+
+//console.log(id);
+//console.log(anotherId);
+
+
+
+// Non-primitive or reference 
+// Array, Objects, Functions
+
+const heros = ["saktiman", "nagraj", "doga"]
+let myobj = {
+    name:"Karan",
+    age:22,
+     
+}
+//console.log(myobj.name);
+
+const myfunction = function(){
+    console.log("hello World");
+}
+myfunction()
+
+
+
+//********************************************Memory*********************************
+
+// There are two types og memory 
+/*
+    i) Stack :- for primitive data types 
+   ii) heap  :- for non primitive data types
+
+*/
+
+
