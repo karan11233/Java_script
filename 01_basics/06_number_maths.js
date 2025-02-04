b2ec104788344eb5d916af839f7bc1917b21016a
karan11233 <karanchavda953@gmail.com>
2025-02-04 12:29:47 +0530

    math

---
+++ 01_basics/06_number_maths.js
@@ -0,0 +1,45 @@
+const score = 100 // one way to declare the number
+// it will create a variable and assign the value in it
+// console.log(typeof score);
+// console.log(score);
+
+
+const balance = new Number(120) // another way to declare number 
+// it will create an object
+// console.log(typeof balance);
+// console.log(balance);
+
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // used to set fixed number after decimal 
+
+
+const otherNumber = 23.37345
+// console.log(otherNumber.toPrecision(2));
+
+const number = 10000000
+// console.log(number.toLocaleString('en-IN'));
+
+// ******************* // Math // ********************** //
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6545)); // it will round of the value                 // Ex:- 3.4456 => 3
+                                                                                //or:- 3.5454 => 4
+
+
+// console.log(Math.ceil(4.6545));  // it will round if the value in upper value  // Ex:- 3.4456 => 3
+// console.log(Math.floor(4.6545)); // it will round of the value in lower value  // Ex:- 3.4456 => 4
+
+
+console.log(Math.random());
+console.log(Math.round((Math.random()*10)))
+
+
+
+
+
+
+
+
