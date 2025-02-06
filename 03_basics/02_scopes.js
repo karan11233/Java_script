93e5c5de71b92c05b61d426c185fe9535e5147e4
karan11233 <karanchavda953@gmail.com>
2025-02-06 15:44:51 +0530

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,6 +5,31 @@ if(true){
     const c = 4
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+function one(){
+    const username =  "Karan"
+
+    function two(){
+        const website = "youtube.com"
+        console.log(username);
+    }
+    // console.log(website);
+    two()    
+} 
+//one()
+
+
+function addOne(num){
+    return num += 1
+}
+console.log(addOne(5))
+
+const addTwo = function(num){
+    return num += 2
+}
+console.log(addTwo(5))
